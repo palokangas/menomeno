613b1bd21ac40abe595df274ffc53345b9817c97
Teemu Palokangas <teemu@palokangas.net>
2019-08-16 15:50:50 +0300

    Make project installable

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,11 +1,25 @@
 import os
-import sys
 import tempfile
 import pytest
 
-# Imports won't work without this, even though pytest finds correct modules
-sys.path.append("../")
-
 from menomeno import create_app, db
 from menomeno.models import Event, Venue, Category, City, Organizer
+
+@pytest.fixture
+def app():
+    db_fd, db_fname = tempfile.mkstemp()
+    config = {
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///" + db_fname,
+        "TESTING": True
+    }
+
+    app = create_app(config)
+
+    with app.app_context():
+        db.create_all()
+    
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_fname)
 
