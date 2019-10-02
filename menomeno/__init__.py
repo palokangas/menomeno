61f333e2f2ffbc0f81996a4622563c11af2acafe
Teemu Palokangas <teemu@palokangas.net>
2019-10-02 12:24:36 +0300

    Add new files and folders, add static folder configuration

--- menomeno/__init__.py
+++ menomeno/__init__.py
@@ -7,18 +7,18 @@ db = SQLAlchemy()
 
 # Based on course material
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder="static")
     app.config.from_mapping(
         SECRET_KEY="dev",
         SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(app.instance_path, "development.db"),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
-    
+
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
-        
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
