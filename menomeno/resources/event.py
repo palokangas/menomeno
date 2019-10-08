8cfa524dab45ff88fd355821691baa1eaaf848d0
Teemu Palokangas <teemu@palokangas.net>
2019-10-08 09:46:13 +0300

    Implement PUT and DELETE for EventItem

--- menomeno/resources/event.py
+++ menomeno/resources/event.py
@@ -81,11 +81,12 @@ class EventCollection(Resource):
             eventdata.append(col.create_data("venue", event_item.venue.name, "Venue of the Event"))
             eventdata.append(col.create_data("venue_url", event_item.venue.url, "Venue URL"))
             eventdata.append(col.create_data("startTime", event_item.startTime.isoformat(), "Start time of the Event"))
+            eventdata.append(col.create_data("category", event_item.category.name, "Category of the Event"))
 
             eventlinks = []
             eventlinks.append(col.create_link("up", up, up_prompt))
 
-            col.add_item(event_item.url, eventdata, eventlinks)
+            col.add_item(url_for("api.eventitem", event_handle=event_item.url), eventdata, eventlinks)
 
             col.add_template_data(col.create_data("name", "", "Event name"))
             col.add_template_data(col.create_data("description", "", "Event description"))
@@ -94,8 +95,6 @@ class EventCollection(Resource):
             col.add_template_data(col.create_data("venue", "", "Start time of the Event"))
             col.add_template_data(col.create_data("city", "", "Start time of the Event"))
 
-
-        print(col)
         return Response(json.dumps(col), 200, mimetype=MIMETYPE)
 
     def post(self):
@@ -179,77 +178,78 @@ class EventCollection(Resource):
 
 class EventItem(Resource):
     """
-    Class representing single venue resource. GET and PUT are supported methods.
+    Class representing single event resource. GET, PUT and DELETE are supported methods.
     """
 
-    def get(self, cityhandle, venue_handle):
+    def get(self, event_handle):
         """
         Creates a response object for GET requests
 
-        : param str cityhandle: name of the city
-        : param str venue_handle: name of the venue
+        : param str event_handle: URI base of the event (autoformatted from name, venue and time)
         """
 
-        city_item = City.query.filter_by(name=cityhandle).first()
-        if city_item is None:
-            return create_error_response(404, "City not found",
-                                "The API can not find the City requested.")
+        event_item = Event.query.filter_by(url=event_handle).first()
+        if event_item is None:
+            return create_error_response(404, "Event not found",
+                                         "The API can not find the Event with the given identifier.")
 
         col = CollectionBuilder()
-
         col_links = []
         col_links.append(col.create_link("profile", PROFILE_URL, "Link to profile"))
-        col_links.append(col.create_link("up", url_for("api.venuecollection", cityhandle=city_item.name), "City"))
+        col.create_collection(url_for("api.eventitem", event_handle=event_handle), col_links)
 
-        col.create_collection(url_for("api.venuecollection", cityhandle=city_item.name), col_links)
+        eventdata = []
+        eventdata.append(col.create_data("name", event_item.name, "Event name"))
+        eventdata.append(col.create_data("description", event_item.description, "Event description"))
+        eventdata.append(col.create_data("organizer", event_item.organizer.name, "Organizer of the Event"))
+        eventdata.append(col.create_data("venue", event_item.venue.name, "Venue of the Event"))
+        eventdata.append(col.create_data("venue_url", event_item.venue.url, "Venue URL"))
+        eventdata.append(col.create_data("startTime", event_item.startTime.isoformat(), "Start time of the Event"))
+        eventdata.append(col.create_data("category", event_item.category.name, "Category of the Event"))
 
-        venue_item = Venue.query.filter_by(name=venue_handle, city_id=city_item.id).first()
-        if venue_item is None:
-            return create_error_response(404, "Venue not found",
-                                         "The API can not find the Venue with the given name in this city.")
+        eventlinks = []
+        eventlinks.append(col.create_link("collection", url_for("api.eventcollection"), "All events"))
+        eventlinks.append(col.create_link("in-venue", url_for("api.venueitem",
+                                                              venue_handle=event_item.venue.name,
+                                                              cityhandle=event_item.venue.city.name),
+                                                              "Venue"))
+        eventlinks.append(col.create_link("organized-by", url_for("api.organizerevents",
+                                                                  organizer_handle=event_item.organizer.name),
+                                                                  "Organizer's other events"))
+        # eventlinks.append(col.create_link("in-city", up, up_prompt))
+        # eventlinks.append(col.create_link("in-category", up, up_prompt))
+        # City-based Events and Categories not implemented yet
 
-        venuedata = []
-        venuedata.append(col.create_data("name",
-                                    venue_item.name,
-                                    prompt="Venue name"))
+        col.add_item(url_for("api.eventitem", event_handle=event_item.url), eventdata, eventlinks)
 
-        venuedata.append(col.create_data("url",
-                                    venue_item.url,
-                                    prompt="Venue URL"))
+        col.add_template_data(col.create_data("name", event_item.name, "Event name"))
+        col.add_template_data(col.create_data("description", event_item.description, "Event description"))
+        col.add_template_data(col.create_data("startTime", event_item.startTime.isoformat(), "Start time of the Event"))
+        col.add_template_data(col.create_data("organizer", event_item.organizer.name, "Organizer of the Event"))
+        col.add_template_data(col.create_data("venue", event_item.venue.name, "Start time of the Event"))
+        col.add_template_data(col.create_data("city", event_item.venue.city.name, "Start time of the Event"))
 
-        venuelinks = []
-        venuelinks.append(col.create_link("events-in",
-                                    url_for("api.venueevents", cityhandle=city_item.name, venue_handle=venue_item.name),
-                                    "Events on this venue"))
-
-        col.add_item(url_for("api.venueitem", cityhandle = city_item.name, venue_handle=venue_item.name),
-                                venuedata,
-                                venuelinks)
-
-        col.add_template_data(col.create_data("name", venue_item.name, "Name of the Venue"))
-        col.add_template_data(col.create_data("url", venue_item.url, "URI of the Venue"))
-
+        print(col)
         return Response(json.dumps(col), 200, mimetype=MIMETYPE)
 
-    def put(self, cityhandle, venue_handle):
+    def put(self, event_handle):
         """
-        Function for editing venue information. Gets values from Request,
+        Function for editing event information. Gets values from Request,
         returns 201 with location header for successful edit and
         error messages for failed edits.
 
-        : param str cityhandle: name of the city
-        : param str venue_handle: name of the venue
+        : param str event_handle: url base of the venue
         """
 
-        city_item = City.query.filter_by(name=cityhandle).first()
-        if city_item is None:
-            return create_error_response(404, "City not found",
-                                "The API can not find the City requested.")
+        event_item = Event.query.filter_by(url=event_handle).first()
+        if event_item is None:
+            return create_error_response(404, "Event not found",
+                                "The API can not find the Event requested.")
 
         if request.method != "PUT":
             return "PUT method required", 405
         else:
-            print("Editing product information")
+            print("Editing Event information")
 
         try:
             json.loads(str(request.json).replace("\'", "\""))
@@ -264,16 +264,7 @@ class EventItem(Resource):
         try:
             req = request.json
             new_name = get_value_for('name', req)
-            new_url = get_value_for('url', req)
-
-            venue_with_same_name = Venue.query.filter_by(name=new_name, city_id=city_item.id).first()
-            oldvenue = Venue.query.filter_by(name=venue_handle, city_id=city_item.id).first()
-            if venue_with_same_name is not None and venue_with_same_name is not oldvenue:
-                return create_error_response(409, "Venue name exists",
-                                             "Trying to assign venue a name that is already a name of another venue in the same city.")
-            if oldvenue is None:
-                return create_error_response(404, "Venue does not exist",
-                                             "The API can not find the Venue in the City requested.")
+            new_description = get_value_for('description', req)
 
         except KeyError:
             return create_error_response(400, "Incomplete request",
@@ -281,27 +272,60 @@ class EventItem(Resource):
 
         except ValueError:
             return create_error_response(400, "Invalid types",
-                            "Weight and price must be numbers")
+                            "JSON body contains invalid information of invalid type.")
 
         try:
-            oldvenue.name = new_name
-            oldvenue.url = new_url
+            event_item.name = new_name
+            event_item.description = new_description
             db.session.commit()
             resp = Response(status=201)
-            resp.headers['location']= url_for('api.venueitem', cityhandle=city_item.name, venue_handle=oldvenue.name)
+            resp.headers['location'] = url_for('api.eventitem', event_handle=event_item.url)
             return resp
         except Exception as e:
             print(e)
             print("New venue cannot be added to database. Rolling back.")
 
+    def delete(self, event_handle):
+        """
+        Function for deleting event information.
+
+        : param str event_handle: url base of the venue
+        """
+
+        event_item = Event.query.filter_by(url=event_handle).first()
+        if event_item is None:
+            return create_error_response(404, "Event not found",
+                                "The API can not find the Event requested.")
+
+        if request.method != "DELETE":
+            return "DELETe method required", 405
+        else:
+            print("Deleting Event information")
+
+        try:
+            db.session.delete(event_item)
+            db.session.commit()
+            return Response(status=204)
+        except Exception as e:
+            print(e)
+            print("Event cannot be deleted. Rolling back")
+            return create_error_response(400, "Database operation failed",
+                                        "Product cannot be deleted. Rolling back.")
+
 
 class VenueEvents(EventCollection):
-
+    """
+    Subclass of EventCollection that uses super methods for everything.
+    Implemented just to expose multiple collections of Events.
+    """
     def get(self, cityhandle, venue_handle):
         return super().get(cityhandle=cityhandle, venue_handle=venue_handle)
 
+
 class OrganizerEvents(EventCollection):
+    """
+    Subclass of EventCollection that uses super methods for everything.
+    Implemented just to expose multiple collections of Events.
+    """
     def get(self, organizer_handle):
-        super.get(organizer_handle=organizer_handle)
-
-
+        return super().get(organizer_handle=organizer_handle)
